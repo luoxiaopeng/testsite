4bccf645281fd8144202afa0cdbd0339ecccb39f
luoxiaopeng <luoxiaopeng@laien.io>
2024-04-18 10:34:24 +0800

    no message

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import permissions
+from adrf.views import APIView, Response
 
 
 class TestApiView(APIView):
-    permission_classes = [permissions.AllowAny]
+    # permission_classes = [permissions.AllowAny]
     authentication_classes = []
 
     def get(self, request, *args, **kwargs):
-        return Response(
-            {"operation": "ok"},
-            status=200,
-        )
+        return Response({"message": "This is an async class based view."})
--- testsite/settings.py
+++ testsite/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
-    'rest_framework',
+    'adrf',
 ]
 
 MIDDLEWARE = [
